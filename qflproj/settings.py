8459134802ebf630c10ce389ac823f1eebea5ebb
srbombaywala <srbombaywala@gmail.com>
2024-10-08 12:53:28 +0530

    adding railway database

--- qflproj/settings.py
+++ qflproj/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'qflproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'vSKjzQiMPTRXuXKjYPdlMJGsaIVSPvom',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
